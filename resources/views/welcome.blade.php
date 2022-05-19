4d5a42bff95020d10364607a1b6d06d3cbb78261
Ashish971 <asisniraula3@gmail.com>
2022-05-19 15:15:14 +0545

    //removed spaces

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="css/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-
 <body>
     <div class="wrapper">
         <div class="title-text">
